df232cd869e775cc5d7c3097bdd0eb95ea999b48
rai <rai002@yahoo.co.jp>
2017-07-16 22:55:16 +0900

    OK

--- testCore/testCore/Controllers/test/testController.cs
+++ testCore/testCore/Controllers/test/testController.cs
@@ -7,40 +7,44 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace testCore.Controllers.test
 {
-    [Produces("application/json")]
-    [Route("api/test")]
-    public class testController : Controller
-    {
-        // GET: api/test
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+	[Produces("application/json")]
+	[Route("api/test")]
+	public class testController : Controller
+	{
+		// GET: api/test
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			//
 
-        // GET: api/test/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-        
-        // POST: api/test
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-        
-        // PUT: api/test/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-        
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-    }
+
+
+			return new string[] { "value1", "value2" };
+		}
+
+		// GET: api/test/5
+		[HttpGet("{id}", Name = "Get")]
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST: api/test
+		[HttpPost]
+		public void Post([FromBody]string value)
+		{
+		}
+
+		// PUT: api/test/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody]string value)
+		{
+		}
+
+		// DELETE: api/ApiWithActions/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
 }
